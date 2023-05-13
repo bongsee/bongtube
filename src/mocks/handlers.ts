f07f442a4e48a5d94948e0a21a707a9993a616ff
bongsee <ajinsuk3@naver.com>
2023-05-13 18:40:05 +0900

    conf: [msw] 실제 api 주소 세팅 및 핸들러 추가
    - hot videos -> '/videos'
    - searching -> '/search'

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,8 +1,16 @@
 import { rest } from "msw"
-import hotVideos from "./data/hotVides.json"
+import hotVideos from "./data/hot.json"
+import searchedVideos from "./data/search.json"
 
 export const handlers = [
-  rest.get("/videos", (req, res, ctx) => {
+  rest.get("https://www.googleapis.com/youtube/v3/videos", (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(hotVideos))
+  }),
+  rest.get("https://www.googleapis.com/youtube/v3/search", (req, res, ctx) => {
+    if (req.url.search.includes("q=")) {
+      return res(ctx.status(200), ctx.json(searchedVideos))
+    } else {
+      return res(ctx.status(200), ctx.json(hotVideos))
+    }
   }),
 ]
