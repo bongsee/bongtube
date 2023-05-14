a83384afb504e11f1a96c0c0c508177fcf0dd1e8
bongsee <ajinsuk3@naver.com>
2023-05-15 02:00:00 +0900

    refactor: 불필요한 routing 구조 개선
    - Root 컴포넌트 제거
    - App 컴포넌트에 통합
    - router -> Router로 이름 수정
    - createRoutesFromElements 메서드 활용하여 가독성 고려

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css"
-import { RouterProvider } from "react-router-dom"
+import { Outlet } from "react-router-dom"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
-import router from "./routes/router"
 import { YoutubeApiProvider } from "./contexts/youtubeApiContext"
+import SearchHeader from "./components/SearchHeader"
 
 console.log("[App.tsx] : has been loaded")
 
@@ -21,10 +21,11 @@ function App() {
   console.log("[App.tsx] : has been rendered")
   return (
     <>
+      <SearchHeader />
       <QueryClientProvider client={queryClient}>
         <ReactQueryDevtools initialIsOpen={true} />
         <YoutubeApiProvider>
-          <RouterProvider router={router} />
+          <Outlet />
         </YoutubeApiProvider>
       </QueryClientProvider>
     </>
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,42 +1,27 @@
-import { createBrowserRouter } from "react-router-dom"
+import {
+  Route,
+  Routes,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom"
 
-import Root from "../pages/Root"
+import App from "../App"
 import HomePage from "../pages/Home"
 import ResultsPage from "../pages/Results"
 import WatchPage from "../pages/Watch"
 import NotFound from "../pages/NotFound"
 
-import { homePageLoader, resultsPageLoader, watchPageLoader } from "./loader"
+const Router = createBrowserRouter(
+  createRoutesFromElements(
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<HomePage />} />
+        <Route path="results" element={<ResultsPage />} />
+        <Route path="watch/:id" element={<WatchPage />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  )
+)
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-        loader: homePageLoader,
-      },
-      {
-        path: "/results",
-        element: <ResultsPage />,
-        loader: resultsPageLoader,
-      },
-      {
-        path: "/watch/:id",
-        element: <WatchPage />,
-        loader: watchPageLoader,
-      },
-    ],
-  },
-  /**
-   * @todo 매치되는 경로가 없을 때 Home으로 redirect 시키는 것도 고려
-   */
-  {
-    path: "*",
-    element: <NotFound />,
-  },
-])
-
-export default router
+export default Router
