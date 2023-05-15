5b3c69f10f8e477b4e9fac6945006f481623d8e4
bongsee <ajinsuk3@naver.com>
2023-05-15 21:22:05 +0900

    feat: RelatedVideos 컴포넌트 기능 구현

---
+++ src/components/RelatedVideos.tsx
@@ -0,0 +1,28 @@
+import { useQuery } from "@tanstack/react-query"
+import React from "react"
+import { useYoutubeApi } from "../contexts/youtubeApiContext"
+import VideoCard from "./VideoCard"
+
+type RelatedVideosProps = {
+  id: string
+}
+
+function RelatedVideos({ id }: RelatedVideosProps) {
+  const { youtube } = useYoutubeApi()
+
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery(["related_videos", id], () => youtube.fetchRelatedVideos(id))
+
+  return (
+    <ul>
+      {videos?.map((video) => (
+        <VideoCard key={video.id as string} video={video} />
+      ))}
+    </ul>
+  )
+}
+
+export default RelatedVideos
